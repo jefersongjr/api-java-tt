96dbc14a75474aecef7f4d1df4784586aa8e4cd0
jefersongjr <jeferson_gjr@hotmail.com>
2023-01-19 11:33:48 -0300

    🎉 Initial commit : Iniciando o projeto

---
+++ api-java-tt/src/main/java/apijavatt/ApiJavaTtApplication.java
@@ -0,0 +1,13 @@
+package apijavatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaTtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaTtApplication.class, args);
+	}
+
+}
