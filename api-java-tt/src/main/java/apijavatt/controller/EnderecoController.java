2de8a9a3440d8782b7ecb300d618b3aa71515f86
jefersongjr <jeferson_gjr@hotmail.com>
2023-01-19 22:11:26 -0300

    :sparkles: feat: criando rota para adicionar endereço

--- api-java-tt/src/main/java/apijavatt/controller/EnderecoController.java
+++ api-java-tt/src/main/java/apijavatt/controller/EnderecoController.java
@@ -1,5 +1,24 @@
 package apijavatt.controller;
 
-public class EnderecoController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import apijavatt.model.entitys.Endereco;
+import apijavatt.model.repositorys.EnderecoRepository;
+
+@RestController
+@RequestMapping("/endereco")
+public class EnderecoController {
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	@RequestMapping(method={RequestMethod.POST,RequestMethod.PUT})
+	public @ResponseBody Endereco novaPessoa(Endereco endereco) {
+		enderecoRepository.save(endereco);
+		return endereco;
+	}
+	
 }
