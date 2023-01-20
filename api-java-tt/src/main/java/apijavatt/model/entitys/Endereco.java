aa8ea5f60b1dc3f97c962da5f47ed93632232b52
jefersongjr <jeferson_gjr@hotmail.com>
2023-01-20 00:49:45 -0300

    :hammer: refactory: excluindo imports desnecessarios

--- api-java-tt/src/main/java/apijavatt/model/entitys/Endereco.java
+++ api-java-tt/src/main/java/apijavatt/model/entitys/Endereco.java
@@ -1,5 +1,6 @@
 package apijavatt.model.entitys;
 
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -7,33 +8,29 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 
 @Entity
 public class Endereco {
-	
+
 	@Id
-	@Column(name="endereco_id")
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
-	
+
 	private String logradouro;
 	private String CEP;
 	private int numero;
 	private String cidade;
 	private int pessoaId;
-	@OneToOne(fetch = FetchType.LAZY, mappedBy= "endereco")
+	@OneToOne(fetch = FetchType.LAZY, mappedBy = "endereco")
 	private Pessoa owner;
-	
-	public Endereco() { 
-		
+
+	public Endereco() {
+
 	}
-	
-	public Endereco(int id, 
-			String logradouro, 
-			String CEP, int numero, 
-			String cidade, int pessoaId) {
+
+	public Endereco(int id, String logradouro, String CEP, int numero, String cidade, int pessoaId) {
 		super();
 		this.id = id;
 		this.logradouro = logradouro;
@@ -42,51 +39,51 @@ public class Endereco {
 		this.cidade = cidade;
 		this.pessoaId = pessoaId;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 	public String getLogradouro() {
 		return logradouro;
 	}
-	
+
 	public void setLogradouro(String logradouro) {
 		this.logradouro = logradouro;
 	}
-	
+
 	public String getCEP() {
 		return CEP;
 	}
-	
+
 	public void setCEP(String CEP) {
 		this.CEP = CEP;
 	}
-	
+
 	public int getNumero() {
 		return numero;
 	}
-	
+
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
-	
+
 	public String getCidade() {
 		return cidade;
 	}
-	
+
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
-	
+
 	public int getPessoaId() {
 		return pessoaId;
 	}
-	
+
 	public void setPessoaId(int pessoaId) {
 		this.pessoaId = pessoaId;
 	}
--- api-java-tt/src/main/java/apijavatt/model/entitys/Pessoa.java
+++ api-java-tt/src/main/java/apijavatt/model/entitys/Pessoa.java
@@ -1,21 +1,21 @@
 package apijavatt.model.entitys;
 
-import java.sql.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 
 @Entity
 public class Pessoa {
 
 	@Id
+	@Column(name="pessoa_id")
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
