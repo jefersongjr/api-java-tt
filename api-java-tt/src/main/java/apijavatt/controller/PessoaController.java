35871202ed81c45a48d68b009442bd27deb500fb
jefersongjr <jeferson_gjr@hotmail.com>
2023-01-20 21:32:42 -0300

    :sparkles: feat: criando validações para o nome

--- api-java-tt/src/main/java/apijavatt/controller/PessoaController.java
+++ api-java-tt/src/main/java/apijavatt/controller/PessoaController.java
@@ -7,14 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import apijavatt.model.entitys.Pessoa;
 import apijavatt.model.repositorys.PessoaRepository;
 import apijavatt.service.PessoaService;
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/pessoas")
--- api-java-tt/src/main/java/apijavatt/service/PessoaService.java
+++ api-java-tt/src/main/java/apijavatt/service/PessoaService.java
@@ -3,17 +3,28 @@ package apijavatt.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import apijavatt.error.InvalidFieldsException;
 import apijavatt.model.entitys.Pessoa;
 import apijavatt.model.repositorys.PessoaRepository;
 
 @Service
 public class PessoaService {
-	
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
+
 	public Pessoa salvar(Pessoa pessoa) {
+		String nome = pessoa.getNome();
+		validarNome(nome);
 		pessoaRepository.save(pessoa);
 		return pessoa;
 	}
+	
+	public void validarNome(String nome) {
+		if (nome == null)
+			throw new InvalidFieldsException("O campo Nome é obrigatório");
+		if (nome.length() <= 2)
+			throw new InvalidFieldsException("O campo Nome tem que ter pelo menos 3 caracteres.");
+	}
+
 }
