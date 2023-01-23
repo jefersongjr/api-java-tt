9cc5aecbefdfa27d2f5b95ac30d8241e205006ad
jefersongjr <jeferson_gjr@hotmail.com>
2023-01-23 22:49:08 +0000

    :hammer: refactor: adicionando status da resposta

--- api-java-tt/src/main/java/apijavatt/controller/PessoaController.java
+++ api-java-tt/src/main/java/apijavatt/controller/PessoaController.java
@@ -3,12 +3,14 @@ package apijavatt.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import apijavatt.model.entitys.Pessoa;
@@ -22,6 +24,7 @@ public class PessoaController {
 	private PessoaService pessoaService;
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public @ResponseBody Pessoa novaPessoa(Pessoa pessoa) {
 		pessoaService.salvar(pessoa);
 		return pessoa;
