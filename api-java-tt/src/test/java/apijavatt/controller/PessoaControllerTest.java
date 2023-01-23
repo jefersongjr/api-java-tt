a94f1d8c1e389842b7ed7b21bc72af8b0204721a
jefersongjr <jeferson_gjr@hotmail.com>
2023-01-23 22:50:49 +0000

    :heavy_check_mark: tests: criando testes para camada controller

--- api-java-tt/src/test/java/apijavatt/controller/PessoaControllerTest.java
+++ api-java-tt/src/test/java/apijavatt/controller/PessoaControllerTest.java
@@ -1,45 +1,50 @@
 package apijavatt.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
 
-import org.assertj.core.api.Assertions;
-import org.junit.Rule;
+import org.springframework.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import apijavatt.error.InvalidFieldsException;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import apijavatt.model.entitys.Pessoa;
 import apijavatt.service.PessoaService;
-import apijavatt.controller.PessoaController;
+import io.restassured.http.ContentType;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
 
-
-@RunWith(SpringRunner.class)
-@DataJpaTest
-@DisplayName("Testes para Pessoa Repository")
-class PessoaControllerTest {
+@DisplayName("Testes da camada Pessoa controller")
+@ActiveProfiles("test")
+@WebMvcTest(PessoaController.class)
+public class PessoaControllerTest {
 	@Autowired
-	private PessoaController controller;
-	@Rule
-	public ExpectedException thrown = ExpectedException.none();
+	private PessoaController pessoaController;
+
+	@MockBean
+	private PessoaService pessoaService;
+
+	@BeforeEach
+	public void setup() {
+		RestAssuredMockMvc.standaloneSetup(pessoaController);
+	}
 
 	@Test
-	@DisplayName("Testa a resposta ao não inserir o nome")
-	void CreateShouldPersitData() {
+	@DisplayName("Testa se uma pessoa é criada corretamente")
+	void CreateShouldPersitData() throws Exception {
 		LocalDate data = LocalDate.of(1991, 02, 11);
-
 		Pessoa pessoa = new Pessoa("Gabriel Barbosa", data, null);
+		when(pessoaController.novaPessoa(ArgumentMatchers.any(Pessoa.class))).thenReturn(pessoa);
 
-		Assertions.assertThat(pessoa.getId()).isNotNull();
-		Assertions.assertThat(pessoa.getNome()).isEqualTo("Gabriel Barbosa");
-		Assertions.assertThat(pessoa.getdataDeNascimento()).isEqualTo(data);
+		RestAssuredMockMvc.given().accept(ContentType.JSON).post("/pessoas");
+		int code = RestAssuredMockMvc.given().accept(ContentType.JSON).when().post("/pessoas").andReturn()
+				.statusCode();
+		
+		 assertEquals(201, code);
 	}
 }
