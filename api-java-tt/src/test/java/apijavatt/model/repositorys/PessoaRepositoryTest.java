696b04127875279c83a589e0bcc2a9d2e49fe132
jefersongjr <jeferson_gjr@hotmail.com>
2023-01-23 01:26:52 +0000

    :heavy_check_mark: tests: testando o repositorio pessoa para listar pessoas

--- api-java-tt/src/test/java/apijavatt/model/repositorys/PessoaRepositoryTest.java
+++ api-java-tt/src/test/java/apijavatt/model/repositorys/PessoaRepositoryTest.java
@@ -1,8 +1,9 @@
 package apijavatt.model.repositorys;
 
-
 import java.time.LocalDate;
+import java.util.List;
 
+import org.apache.commons.collections4.IterableUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -43,11 +44,51 @@ class PessoaRepositoryTest {
 		pessoaRepository.save(pessoa);
 		pessoa.setNome("Gabriel Henrique");
 		pessoa.setdataDeNascimento(data2);
-		
+
 		pessoa = pessoaRepository.save(pessoa);
 
 		Assertions.assertThat(pessoa.getNome()).isEqualTo("Gabriel Henrique");
 		Assertions.assertThat(pessoa.getdataDeNascimento()).isEqualTo(data2);
+	}
+
+	@Test
+	@DisplayName("Testa se as pessoas são listadas corretamente")
+	void FindAllContainigShould() {
+		LocalDate data = LocalDate.of(1991, 02, 11);
+		LocalDate data2 = LocalDate.of(1993, 02, 10);
+		LocalDate data3 = LocalDate.of(1990, 12, 10);
+
+		Pessoa pessoa = new Pessoa("Gabriel Barbosa", data, null);
+		Pessoa pessoa2 = new Pessoa("Bruno Henrique", data2, null);
+		Pessoa pessoa3 = new Pessoa("Everto Ribeiro", data3, null);
+
+		pessoaRepository.save(pessoa);
+		pessoaRepository.save(pessoa2);
+		pessoaRepository.save(pessoa3);
+		
+		Iterable<Pessoa> pessoaLista = pessoaRepository.findAll();
+
+		Assertions.assertThat(IterableUtils.size(pessoaLista)).isEqualTo(3);
+	}
+	
+	@Test
+	@DisplayName("Testa se a pesquisa por nome é listada corretamente")
+	void FindByNameIgnoreCaseContainigShould() {
+		LocalDate data = LocalDate.of(1991, 02, 11);
+		LocalDate data2 = LocalDate.of(1993, 02, 10);
+		LocalDate data3 = LocalDate.of(1990, 12, 10);
+
+		Pessoa pessoa = new Pessoa("Gabriel Barbosa", data, null);
+		Pessoa pessoa2 = new Pessoa("Bruno Henrique", data2, null);
+		Pessoa pessoa3 = new Pessoa("Gabriel Ribeiro", data3, null);
+
+		pessoaRepository.save(pessoa);
+		pessoaRepository.save(pessoa2);
+		pessoaRepository.save(pessoa3);
+		
+		Iterable<Pessoa> pessoaLista = pessoaRepository.findByNomeContainingIgnoreCase("Gabriel");
+
+		Assertions.assertThat(IterableUtils.size(pessoaLista)).isEqualTo(2);
 	}
 
 }
